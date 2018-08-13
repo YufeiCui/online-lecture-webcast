1bd9d4b171842ae56330f24c2bb0525e5a87d58d
Yufei Cui <yufei.cui@ibm.com>
2018-08-13 19:13:37 -0400

    Fix Typo and reformat file for PEP8

--- download.py
+++ download.py
@@ -1,16 +1,17 @@
 import urllib.request
-import os
+
 
 class InvalidValueException(Exception):
     pass
 
-def construct(formula, configuration, concatenator = '/', escape_prefix = '/'):
+
+def construct(formula, configuration, concatenator='/', escape_prefix='/'):
     '''
     (list, dict, [,str[,str]]) -> str
     '''
 
     # simple function to concate strings with a concatenator in between
-    def concatenate_item(before, item, concatenator = concatenator):
+    def concatenate_item(before, item, concatenator=concatenator):
         return before + concatenator + item
 
     EXTENSION_KEYWORD = 'extension'
@@ -20,7 +21,7 @@ def construct(formula, configuration, concatenator = '/', escape_prefix = '/'):
     # look at each term in the formula
     for term in formula:
         # treatment for a string
-        if (isinstance(term, str)):
+        if isinstance(term, str):
             # if the item starts with the escape prefix, then do not evaulate
             if term.startswith(escape_prefix):
                 rs = concatenate_item(rs, term[1:])
@@ -36,12 +37,12 @@ def construct(formula, configuration, concatenator = '/', escape_prefix = '/'):
                     rs = concatenate_item(rs, term)
         # if there's another formula
         elif isinstance(term, list):
-            if (len(term[-1]) == 1):
+            if len(term[-1]) == 1:
                 new_concatenator = term[-1]
             else:
                 new_concatenator = concatenator
-            # concanate current string and the result string from the new formula
-            rs = concatenate_item(rs, construct(term[:-1], configuration, concatenator = new_concatenator))
+            # concatenate current string and the result string from the new formula
+            rs = concatenate_item(rs, construct(term[:-1], configuration, concatenator=new_concatenator))
         # cast ints or floats
         elif isinstance(term, int) or isinstance(term, float):
             rs = concatenate_item(term, str(term))
@@ -51,7 +52,8 @@ def construct(formula, configuration, concatenator = '/', escape_prefix = '/'):
     # add/remove extra concatenators
     return rs[1:]
 
-def download(where, to, name = None):
+
+def download(where, to, name=None):
     # construct the name that the file will be stored as
     if name:
         constructed_name = to + name
@@ -60,6 +62,7 @@ def download(where, to, name = None):
 
     urllib.request.urlretrieve(where, constructed_name)
 
+
 if __name__ == "__main__":
     import json
 
@@ -67,7 +70,8 @@ if __name__ == "__main__":
     config = json.load(open("./config.json", "r"))
 
     # generates a url given the formula and the configured keys
-    url = construct(config["Constrution"]["url"], config["DownloadSettings"], concatenator = '/', escape_prefix = '/')
+    url = construct(config["Construction"]["url"], config["DownloadSettings"], concatenator='/', escape_prefix='/')
 
+    print(url)
     # given the url, load the download directory and download the content
-    download(url, config["DownloadSettings"]["location"])
+    # download(url, config["DownloadSettings"]["location"])
